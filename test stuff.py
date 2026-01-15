cd0a85d859c87f8be61804ee9f92f896951bd7a6
OnlyX-ma <abdallah360abdallah@gmail.com>
2026-01-15 17:35:12 +0100

    myfirstcode

---
+++ test stuff.py
@@ -0,0 +1,28 @@
+# these are strings
+friend_name  = "vinz"
+stuff_he_likes = "goth girls"
+
+print (f"hello {friend_name}")
+print (f"you seem to love {stuff_he_likes}")
+
+# integer
+age = 67
+print(f"you are {age} years old fuck")
+quantity = 0
+print(f"you have {quantity} girlfreinds so far")
+
+# floats
+price = 10.99
+print(f"you spending ${price} a day on games")
+grass_time = 0.0001
+print(f"you touch grass {grass_time}sec a day")
+print("go out and explore")
+
+# boolean                               ass name btw
+gay = True
+print(f"am i gay? {gay}")
+if gay: 
+    print("yes you are gay")
+else:
+    print("you are not gay")
+    
